c1c4f76380c24199c82fe13e8a6dd08228764970
kplissonneau <kp1711@ic.ac.uk>
2014-06-06 00:08:22 +0100

    small calibration update arduino

--- MUX8codeDigitalWrite/MUX8codeDigitalWrite.ino
+++ MUX8codeDigitalWrite/MUX8codeDigitalWrite.ino
@@ -8,8 +8,20 @@ int S2 = 5;
 int S3 = 4;
 
 int muxValues[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-int muxThresholds[] = {310,250,400,370,340,380,360,310,295,430};
+int muxThresholds[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};//= {310,250,400,370,340,380,360,310,295,430};
 
+// digital values to control 8 inputs
+int d_bin[]={LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,HIGH,HIGH,HIGH,HIGH,HIGH,HIGH,HIGH,HIGH};
+int c_bin[]={LOW,LOW,LOW,LOW,HIGH,HIGH,HIGH,HIGH,LOW,LOW,LOW,LOW,HIGH,HIGH,HIGH,HIGH};
+int b_bin[]={LOW,LOW,HIGH,HIGH,LOW,LOW,HIGH,HIGH,LOW,LOW,HIGH,HIGH,LOW,LOW,HIGH,HIGH};
+int a_bin[]={LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH};
+
+int i=0;
+int a=0;
+int a_val=0;
+int b_val=0;
+int c_val=0;
+int d_val=0;
 
   
 void setup()
@@ -26,38 +38,11 @@ void setup()
   lcd.setCursor(4,2); lcd.print("Dienes Blocks!");
   delay(2000);
   lcd.clear();
+  
+  calibration();
+ 
 }
  
- void displayData()
-// dumps captured data from array to serial monitor
-{
-  Serial.println();
-  Serial.println("Values from multiplexer:");
-  Serial.println("========================");
-  for (int i = 0; i < 10; i++)
-  {
-    Serial.print("input I");
-    Serial.print(i);
-    Serial.print(" = ");
-    Serial.println(muxValues[i]);
-  }
-  Serial.println("========================");  
-}
-
-
-
-// digital values to control 8 inputs
-int d_bin[]={LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,HIGH,HIGH,HIGH,HIGH,HIGH,HIGH,HIGH,HIGH};
-int c_bin[]={LOW,LOW,LOW,LOW,HIGH,HIGH,HIGH,HIGH,LOW,LOW,LOW,LOW,HIGH,HIGH,HIGH,HIGH};
-int b_bin[]={LOW,LOW,HIGH,HIGH,LOW,LOW,HIGH,HIGH,LOW,LOW,HIGH,HIGH,LOW,LOW,HIGH,HIGH};
-int a_bin[]={LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH};
-
-int i=0;
-int a=0;
-int a_val=0;
-int b_val=0;
-int c_val=0;
-int d_val=0;
 
 void loop()
 {
@@ -102,17 +87,54 @@ void loop()
   
   lcd.setCursor(0, 3); lcd.print("0"); lcd.setCursor(2, 3);lcd.print(" Thousands"); 
     
-    Serial.print(" "); 
+    //Serial.print(" "); 
     Serial.print("0"); Serial.print(" "); 
     Serial.print(a); Serial.print(" "); 
     Serial.print("0"); Serial.print(" "); 
     Serial.print("0"); 
     
     delay(1000);
-  
+  displayData();
 
   
   
   //compare each input to threshold, if > increment counter  
   
 }
+
+ void displayData()
+// dumps captured data from array to serial monitor
+{
+  Serial.println();
+  Serial.println("Values from multiplexer:");
+  Serial.println("========================");
+  for (int i = 0; i < 10; i++)
+  {
+    Serial.print("input I");
+    Serial.print(i);
+    Serial.print(" = ");
+    Serial.println(muxValues[i]);
+  }
+  Serial.println("========================");  
+}
+
+ //calibration of sensors (compensate for temperature errors). PLatform needs to be empty
+ void calibration() {
+   
+   for(i=0;i<=9;i++) {
+    //select mux input
+    a_val=a_bin[i];
+    b_val=b_bin[i];
+    c_val=c_bin[i];
+    d_val=d_bin[i];
+    digitalWrite(S0,a_val);
+    digitalWrite(S1,b_val);
+    digitalWrite(S2,c_val);
+    digitalWrite(S3,d_val);
+    muxThresholds[i] = analogRead(0);
+    muxThresholds[i] = muxThresholds[i] + 15;
+    Serial.println(muxThresholds[i]);
+
+    delay(100);
+   }
+ }
